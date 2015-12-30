7193d9f937da98b3518e117444c6ea3d6ee29655
louisfaury <l.faury@hotmail.fr>
2015-12-29 19:31:36 -0500

    Creating config for Autopilot

---
+++ Config.hpp
@@ -0,0 +1,31 @@
+//
+//  Config.hpp
+//  Auto_Pilot
+//
+//  Created by Louis Faury on 29/12/2015.
+//  Copyright © 2015 Louis Faury. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+
+// For Autopilot Block
+
+#define MAX_ALTI_TARGET 2000 //cm
+#define INI_ALTI_TARGET 50 //cm
+
+#define MIN_TIME_RATE 5 //ms
+#define INTERP_TIME 5000 //ms
+
+
+
+
+
+
+
+
+
+
+
+#endif
