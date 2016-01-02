4dcd2747f417465f517309512629b18f73ad0e10
louisfaury <l.faury@hotmail.fr>
2016-01-02 16:23:23 +0100

    Testing PID on simple system

--- main.cpp
+++ main.cpp
@@ -7,15 +7,30 @@
 //
 
 #include <iostream>
-#include "Autopilot.h"
+#include "Autopilot.hpp"
 #include "util.hpp"
+#include "PID.hpp"
+#include <fstream>
 
 
 int main(int argc, const char * argv[]) {
     
     
-    Autopilot _auto(10); // Autopilot with 10 ms time rate
-    _auto._update();
-
-    return 1;
+    /*Autopilot _auto(10); // Autopilot with 10 ms time rate
+    _auto.take_off();
+    _auto._run(); */
+    
+    const std::string file_path = "/Users/louisfaury/Documents/C++/Auto_pilot/test.txt";
+    std::ofstream file(file_path.c_str());
+                        
+    PID pid(10,-2,-0.01,-0.1);
+    pid.sum_up();
+    pid._update(0,1);
+    // Test and simulate PID controler for \dot{x) = u
+    for (int i=0; i<1000; i++) {
+        file << pid.sum_up() << std::endl;
+        pid._update(pid.getState() + pid.calculate_command()/100); // See what to divide the command by .. (ordre de grandeur ?) // TODO
+        usleep(1000);
+    }
+    return 0;
 }
