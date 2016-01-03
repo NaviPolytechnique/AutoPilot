35b08ca29e3d27ea987aeccafd1dbcb43c228f5f
louisfaury <l.faury@hotmail.fr>
2016-01-03 17:15:03 +0100

    Creating file

---
+++ Motors.hpp
@@ -0,0 +1,32 @@
+//
+//  Motors.hpp
+//  Auto_Pilot
+//
+//  Created by Louis Faury on 03/01/2016.
+//  Copyright © 2016 Louis Faury. All rights reserved.
+//
+
+#ifndef Motors_hpp
+#define Motors_hpp
+
+#include <stdio.h>
+#include "util.hpp"
+#include "Config.hpp"
+// TODO : add DC motor library (PMW)
+
+
+class Motors {
+    
+public:
+    
+    Motors();
+    
+    void _update(Eigen::Vector4f); // Updates propeller speed given a command 
+    
+private:
+    
+    Eigen::Vector4f _motor_speed;
+    // TODO : Port to write on ?
+    
+};
+#endif /* Motors_hpp */
